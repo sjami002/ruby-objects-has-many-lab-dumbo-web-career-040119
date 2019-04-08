a5fb83659ceaa570823242f576090cf287ded865
Shoptorshi Jamil <sjami002@gmail.com>
2019-04-08 01:53:39 +0000

    Automatically backed up by Learn

---
+++ lib/author.rb
@@ -0,0 +1,28 @@
+class Author
+  
+  attr_accessor :name
+  
+  def initialize(name)
+    @name = name
+    @posts = []
+  end
+  
+  def posts
+    @posts
+  end
+  
+  def add_post(post)
+    @posts << post
+    post.author = self
+  end
+  
+  def add_post_by_title(title)
+    
+    title = Post.new(title)
+    @posts << title
+    title.author = self
+    
+  end
+  
+  
+end
